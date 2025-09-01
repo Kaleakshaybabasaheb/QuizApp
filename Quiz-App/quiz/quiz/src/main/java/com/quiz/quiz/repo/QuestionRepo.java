25bbafb113d0a66bff1522f6f1bf1148f1641eeb
Kaleakshaybabasaheb <91794791+Kaleakshaybabasaheb@users.noreply.github.com>
2025-09-02 00:16:01 +0530

    Delete Quiz-App directory

--- Quiz-App/quiz/quiz/src/main/java/com/quiz/quiz/repo/QuestionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.quiz.quiz.repo;
-
-import com.quiz.quiz.entity.QuizQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepo extends JpaRepository <QuizQuestion, Long>{
-}
